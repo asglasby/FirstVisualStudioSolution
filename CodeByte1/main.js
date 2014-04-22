7b8915d30ec1bdd134f426a95c6d934b6547304a
asglasby <aishaextra@gmail.com>
2014-04-22 11:31:06 -0400

    Initial Commit, Created Project, added CSS, HTML, and Javascript files

---
+++ CodeByte1/main.js
@@ -0,0 +1,57 @@
+﻿function firstReverse(str) {
+    var str= "Hello My Name is Aisha";
+    
+    return str.split("").reverse().join("");
+
+}
+alert(firstReverse());
+
+var firstFactorial= function(num) {
+
+    if (num === 1)
+    {
+        return 1;
+    }
+     return num * firstFactorial( num - 1);
+} 
+alert(firstFactorial(5));
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+//FirstFactorial(readline());
+
+
+function LongestWord(sen) {
+
+    // code goes here  
+    return sen;
+
+}
+
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+LongestWord(readline());
+
+
+function LetterChanges(str) {
+
+    // code goes here 
+    var letterElements = str.length;
+    var matchfound = "no";
+    var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+
+    for (var i = 0; i <= lengthOfString; i + +) { 
+        if (letterToCheck = = = alphabetLetters[ i]) { 
+            matchFound = "yes";  
+            alert(" It's one of the cleanest cities"); 
+        }  
+    }
+
+     
+    return str;
+
+}
+
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+LetterChanges(readline());
---
+++ CoderByte4/main.js
@@ -0,0 +1,51 @@
+﻿//function LetterChanges(str) {
+
+//    // code goes here 
+//    var letterToCheck;
+//    var numChars = str.length;
+//    var matchfound = false;
+//    var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+//                    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+
+//    for (var i = 0; i < numChars; i ++) { 
+//        if (letterToCheck === alphabetLetters[i]) { 
+//            matchFound = true;
+//            if (matchFound = true) {
+//                letterToCheck = alphabetLetter[i + 1]
+//            }
+//            if (letterToCheck === "a", "e", "i", "o", "u") {
+//                letterToCheck = letterToCheck.toUpperCase();
+//            }
+//            alert(letterToCheck); 
+//        }  
+//    }
+
+     
+//    return str;
+
+//}
+
+//// keep this function call here 
+//// to see how to enter arguments in JavaScript scroll down
+//LetterChanges(readline());
+
+//var str = prompt("Enter your string");
+var str = "The coder school assignment"
+var numChars = str.length;
+var firstChar = str.slice(0, 1);
+var otherChars = str.slice(1);
+//alert(otherChars + "\n" + str + firstChar);
+var newStr = firstChar;
+alert(newStr);
+
+var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+
+for (var i = 0; i < numChars; i++) {
+    if (newStr === alphabetLetters[i]) {
+        matchFound = true;
+        if (matchFound = true) {
+            letterToCheck = alphabetLetter[i + 1]
+        }
+    }
+}
